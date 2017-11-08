d1df26eb8e6289100e8709b7146c6feaadec8699
Annijah <32313026+Annijah@users.noreply.github.com>
2017-11-08 17:45:05 -0500

    More changes

--- volunteer.js
+++ volunteer.js
@@ -1,13 +1,14 @@
-(function(){
-  $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
-  );
-})();
+$(document).ready(function(){
+   $('.materialboxed').materialbox();
+ });
+ $(document).ready(function(){
+     $('.slider').slider();
+   });
+   // Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
